82a8e09b8b112c26e2c7d9f29428862ed5a952bd
Oliver Catling <olli_catling@hotmail.co.uk>
2018-10-01 21:31:50 +0100

    Now loads index template

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
+from .models import Project
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Hello World')
+    projects = Project.objects.all()
+    template = loader.get_template('portfolio/index.html')
+    context = {
+        'all_projects': projects
+    }
+    return HttpResponse(template.render(context, request))
