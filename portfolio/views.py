af0d22d88e01b12c6bfd82dbe28c7883d040f4a7
Oliver Catling <olli_catling@hotmail.co.uk>
2018-10-01 22:17:34 +0100

    Changed How View Is Rendered

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
-from django.template import loader
-from django.http import HttpResponse
 from .models import Project
 
 # Create your views here.
 
 
 def index(request):
-    projects = Project.objects.all()
-    template = loader.get_template('portfolio/index.html')
     context = {
-        'all_projects': projects
+        'projects': Project.objects.all(),
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'portfolio/index.html', context)
