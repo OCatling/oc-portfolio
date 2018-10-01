a56af7b7701e2812689617e2fadd02ec37ed0f47
Oliver Catling <olli_catling@hotmail.co.uk>
2018-10-01 15:12:08 +0100

    Updated Database Config

--- oliver_catling/settings.py
+++ oliver_catling/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'oliver_catling.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'oc',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
