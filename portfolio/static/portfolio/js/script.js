59440733f9f1a12ed5d423e3332015a0520d52cd
Oliver Catling <olli_catling@hotmail.co.uk>
2018-12-10 15:13:23 +0000

    Style Changes

--- portfolio/static/portfolio/js/script.js
+++ portfolio/static/portfolio/js/script.js
@@ -1,15 +1,3 @@
 $(document).ready(function () {
-    // Init Sidebar
-    $(".sidebar.top").sidebar({side: "top"});
-    $(".sidebar.bottom").sidebar({side: "bottom"});
-    $(".sidebar.right").sidebar({side: "right"});
-
-    // Click Handler
-    $(".trigger[data-action]").on("click", function () {
-        var $this = $(this);
-        var action = $this.attr("data-action");
-        var side = $this.attr("data-side");
-        $(".sidebar." + side).trigger("sidebar:" + action);
-        return false;
-    });
+    $()
 });
